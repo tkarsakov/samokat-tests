d2c33c5de575940069a6e2d31a303ef72f5fcffb
krskv <karsakovt89@gmail.com>
2024-05-19 21:42:54 +0300

    Refactor config and testdata services into one

--- src/main/java/com/intexsoft/samokat/service/ResourcesService.java
+++ src/main/java/com/intexsoft/samokat/service/ResourcesService.java
@@ -8,9 +8,12 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class TestdataService {
+public class ResourcesService {
     private static final Path TESTDATA_PATH = Path.of("src/test/resources/testdata.json");
+    private static final Path CONFIG_PATH = Path.of("src/test/resources/config.json");
     private static final JsonObject TESTDATA;
+    private static final JsonObject CONFIG;
+
 
     static {
         try {
@@ -20,4 +23,12 @@ public class TestdataService {
         }
     }
 
+    static {
+        try {
+            CONFIG = new Gson().fromJson(Files.readString(CONFIG_PATH, StandardCharsets.UTF_8), JsonObject.class);
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("Cannot find config.json at %s", CONFIG_PATH));
+        }
+
+    }
 }
